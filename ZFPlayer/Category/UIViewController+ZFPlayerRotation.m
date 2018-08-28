1591a96e64ffe31a8528a2facfaf7e8a4c9c7a77
ungacy <ungacy@126.com>
2018-08-28 14:33:13 +0800

    version bump to 4.0.3

--- ZFPlayer/Category/UIViewController+ZFPlayerRotation.m
+++ ZFPlayer/Category/UIViewController+ZFPlayerRotation.m
@@ -26,32 +26,32 @@
 
 @implementation UIViewController (ZFPlayerRotation)
 
-/**
- * 默认所有都不支持转屏,如需个别页面支持除竖屏外的其他方向，请在viewController重新下边这三个方法
- */
-
-// 是否支持自动转屏
-- (BOOL)shouldAutorotate {
-    return NO;
-}
-
-// 支持哪些屏幕方向
-- (UIInterfaceOrientationMask)supportedInterfaceOrientations {
-    return UIInterfaceOrientationMaskPortrait;
-}
-
-// 默认的屏幕方向（当前ViewController必须是通过模态出来的UIViewController（模态带导航的无效）方式展现出来的，才会调用这个方法）
-- (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation {
-    return UIInterfaceOrientationPortrait;
-}
-
-- (UIStatusBarStyle)preferredStatusBarStyle {
-    return UIStatusBarStyleDefault; // your own style
-}
-
-- (BOOL)prefersStatusBarHidden {
-    return NO; // your own visibility code
-}
+///**
+// * 默认所有都不支持转屏,如需个别页面支持除竖屏外的其他方向，请在viewController重新下边这三个方法
+// */
+//
+//// 是否支持自动转屏
+//- (BOOL)shouldAutorotate {
+//    return NO;
+//}
+//
+//// 支持哪些屏幕方向
+//- (UIInterfaceOrientationMask)supportedInterfaceOrientations {
+//    return UIInterfaceOrientationMaskPortrait;
+//}
+//
+//// 默认的屏幕方向（当前ViewController必须是通过模态出来的UIViewController（模态带导航的无效）方式展现出来的，才会调用这个方法）
+//- (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation {
+//    return UIInterfaceOrientationPortrait;
+//}
+//
+//- (UIStatusBarStyle)preferredStatusBarStyle {
+//    return UIStatusBarStyleDefault; // your own style
+//}
+//
+//- (BOOL)prefersStatusBarHidden {
+//    return NO; // your own visibility code
+//}
 
 @end
 
@@ -63,15 +63,15 @@
     };
     
     for (NSUInteger index = 0; index < sizeof(selectors) / sizeof(SEL); ++index) {
-        SEL originalSelector = selectors[index];
-        SEL swizzledSelector = NSSelectorFromString([@"zf_" stringByAppendingString:NSStringFromSelector(originalSelector)]);
-        Method originalMethod = class_getInstanceMethod(self, originalSelector);
-        Method swizzledMethod = class_getInstanceMethod(self, swizzledSelector);
-        if (class_addMethod(self, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod))) {
-            class_replaceMethod(self, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
-        } else {
-            method_exchangeImplementations(originalMethod, swizzledMethod);
-        }
+//        SEL originalSelector = selectors[index];
+//        SEL swizzledSelector = NSSelectorFromString([@"zf_" stringByAppendingString:NSStringFromSelector(originalSelector)]);
+//        Method originalMethod = class_getInstanceMethod(self, originalSelector);
+//        Method swizzledMethod = class_getInstanceMethod(self, swizzledSelector);
+//        if (class_addMethod(self, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod))) {
+//            class_replaceMethod(self, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
+//        } else {
+//            method_exchangeImplementations(originalMethod, swizzledMethod);
+//        }
     }
 }
 
@@ -87,37 +87,37 @@
  */
 
 // 是否支持自动转屏
-- (BOOL)shouldAutorotate {
-    UIViewController *vc = self.viewControllers[self.selectedIndex];
-    if ([vc isKindOfClass:[UINavigationController class]]) {
-        UINavigationController *nav = (UINavigationController *)vc;
-        return [nav.topViewController shouldAutorotate];
-    } else {
-        return [vc shouldAutorotate];
-    }
-}
-
-// 支持哪些屏幕方向
-- (UIInterfaceOrientationMask)supportedInterfaceOrientations {
-    UIViewController *vc = self.viewControllers[self.selectedIndex];
-    if ([vc isKindOfClass:[UINavigationController class]]) {
-        UINavigationController *nav = (UINavigationController *)vc;
-        return [nav.topViewController supportedInterfaceOrientations];
-    } else {
-        return [vc supportedInterfaceOrientations];
-    }
-}
-
-// 默认的屏幕方向（当前ViewController必须是通过模态出来的UIViewController（模态带导航的无效）方式展现出来的，才会调用这个方法）
-- (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation {
-    UIViewController *vc = self.viewControllers[self.selectedIndex];
-    if ([vc isKindOfClass:[UINavigationController class]]) {
-        UINavigationController *nav = (UINavigationController *)vc;
-        return [nav.topViewController preferredInterfaceOrientationForPresentation];
-    } else {
-        return [vc preferredInterfaceOrientationForPresentation];
-    }
-}
+//- (BOOL)shouldAutorotate {
+//    UIViewController *vc = self.viewControllers[self.selectedIndex];
+//    if ([vc isKindOfClass:[UINavigationController class]]) {
+//        UINavigationController *nav = (UINavigationController *)vc;
+//        return [nav.topViewController shouldAutorotate];
+//    } else {
+//        return [vc shouldAutorotate];
+//    }
+//}
+//
+//// 支持哪些屏幕方向
+//- (UIInterfaceOrientationMask)supportedInterfaceOrientations {
+//    UIViewController *vc = self.viewControllers[self.selectedIndex];
+//    if ([vc isKindOfClass:[UINavigationController class]]) {
+//        UINavigationController *nav = (UINavigationController *)vc;
+//        return [nav.topViewController supportedInterfaceOrientations];
+//    } else {
+//        return [vc supportedInterfaceOrientations];
+//    }
+//}
+//
+//// 默认的屏幕方向（当前ViewController必须是通过模态出来的UIViewController（模态带导航的无效）方式展现出来的，才会调用这个方法）
+//- (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation {
+//    UIViewController *vc = self.viewControllers[self.selectedIndex];
+//    if ([vc isKindOfClass:[UINavigationController class]]) {
+//        UINavigationController *nav = (UINavigationController *)vc;
+//        return [nav.topViewController preferredInterfaceOrientationForPresentation];
+//    } else {
+//        return [vc preferredInterfaceOrientationForPresentation];
+//    }
+//}
 
 @end
 
@@ -128,41 +128,41 @@
  * 只需要在支持除竖屏以外方向的页面重新下边三个方法
  */
 
-// 是否支持自动转屏
-- (BOOL)shouldAutorotate {
-    return [self.topViewController shouldAutorotate];
-}
-
-// 支持哪些屏幕方向
-- (UIInterfaceOrientationMask)supportedInterfaceOrientations {
-    return [self.topViewController supportedInterfaceOrientations];
-}
-
-// 默认的屏幕方向（当前ViewController必须是通过模态出来的UIViewController（模态带导航的无效）方式展现出来的，才会调用这个方法）
-- (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation {
-    return [self.topViewController preferredInterfaceOrientationForPresentation];
-}
-
-- (UIViewController *)childViewControllerForStatusBarStyle {
-    return self.topViewController;
-}
-
-- (UIViewController *)childViewControllerForStatusBarHidden {
-    return self.topViewController;
-}
+//// 是否支持自动转屏
+//- (BOOL)shouldAutorotate {
+//    return [self.topViewController shouldAutorotate];
+//}
+//
+//// 支持哪些屏幕方向
+//- (UIInterfaceOrientationMask)supportedInterfaceOrientations {
+//    return [self.topViewController supportedInterfaceOrientations];
+//}
+//
+//// 默认的屏幕方向（当前ViewController必须是通过模态出来的UIViewController（模态带导航的无效）方式展现出来的，才会调用这个方法）
+//- (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation {
+//    return [self.topViewController preferredInterfaceOrientationForPresentation];
+//}
+//
+//- (UIViewController *)childViewControllerForStatusBarStyle {
+//    return self.topViewController;
+//}
+//
+//- (UIViewController *)childViewControllerForStatusBarHidden {
+//    return self.topViewController;
+//}
 
 @end
 
 @implementation UIAlertController (ZFPlayerRotation)
 
-#if __IPHONE_OS_VERSION_MAX_ALLOWED < 90000
-- (NSUInteger)supportedInterfaceOrientations; {
-    return UIInterfaceOrientationMaskAll;
-}
-#else
-- (UIInterfaceOrientationMask)supportedInterfaceOrientations {
-    return UIInterfaceOrientationMaskAll;
-}
-#endif
+//#if __IPHONE_OS_VERSION_MAX_ALLOWED < 90000
+//- (NSUInteger)supportedInterfaceOrientations; {
+//    return UIInterfaceOrientationMaskAll;
+//}
+//#else
+//- (UIInterfaceOrientationMask)supportedInterfaceOrientations {
+//    return UIInterfaceOrientationMaskAll;
+//}
+//#endif
 
 @end
